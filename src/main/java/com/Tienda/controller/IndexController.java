cfc52d48a1f08819b378508d504a70442d317cc5
yusme <yusme@DESKTOP-HRCEHGL>
2024-10-14 00:41:15 -0600

    Finalizacion semana 04

---
+++ src/main/java/com/Tienda/controller/IndexController.java
@@ -0,0 +1,21 @@
+package com.Tienda.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+    
+    @RequestMapping("/")
+    public String paginaInicio(Model model) {
+       // model.addAttribute("attribute", "value");
+        return "index";
+    }
+    @RequestMapping("/contacto")
+    public String paginaContacto(Model model) {
+       // model.addAttribute("attribute", "value");
+        return "info";
+    }
+    
+}
